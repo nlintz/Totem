f7984145574f0fbae63dc185b6163f90478a4368
Nathan Lintz <nlintz@gmail.com>
2013-06-10 12:55:31 -0700

    first commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Totem::Application.config.secret_key_base = '1a0698bcef1e4a992ba522993e06fd044943b1d7ba2e4ec700895e1dbe5edc1d454fa28bdc92e20684dad2f55295fe370ec2cfc9cebe49eef87c17a3c13419da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Totem::Application.config.session_store :encrypted_cookie_store, key: '_Totem_session'
