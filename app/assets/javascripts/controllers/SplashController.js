ef8fc344902b59cc698f3a026dc38f89ee5513b0
Nathan Lintz <nlintz@gmail.com>
2013-07-07 13:53:50 -0700

    added mixins for animation

--- app/assets/javascripts/controllers/SplashController.js
+++ app/assets/javascripts/controllers/SplashController.js
@@ -1,4 +1,7 @@
 SplashControllers = angular.module('SplashControllers', []);
 SplashControllers.controller('SplashController', ['$scope', function($scope) {
-    
+    $scope.transition_to_login = "false";
+    $scope.transition = function(){
+    	$scope.transition_to_login = 'btn-totem-animation'
+    }
 }]);
